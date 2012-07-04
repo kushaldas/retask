256a11dff9dea796736b75f1bfd1e9dd5ac2476f
Kushal Das <kushaldas@gmail.com>
2012-07-04 16:55:26 +0530

    Release details for the project

---
+++ retask/release.py
@@ -0,0 +1,11 @@
+NAME = 'retask'
+VERSION = '0.1'
+DESCRIPTION = 'Task Queue implementation in python'
+LONG_DESCRIPTION = '''Retask is a simple task queue implementation written for human beings. It provides generic solution to create and manage task queues.'''
+AUTHOR = 'Kushal Das'
+EMAIL = 'kushaldas@gmail.com'
+COPYRIGHT = '2012 Kushal Das'
+URL = 'https://github.com/kushaldas/retask'
+LICENSE = 'MIT'
+
+__all__ = ('NAME', 'VERSION', 'DESCRIPTION', 'LONG_DESCRIPTION', 'AUTHOR', 'EMAIL', 'COPYRIGHT', 'URL', 'LICENSE')

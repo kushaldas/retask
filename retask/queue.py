44860e2a1d89721fa9c8cfd490232da3818c5b5e
Kushal Das <kushaldas@gmail.com>
2012-11-30 16:11:14 +0530

    Cleans lint issues from sublimetext

--- retask/queue.py
+++ retask/queue.py
@@ -29,10 +29,8 @@ retask Queue implementation
 
 """
 import redis
-import json
 from task import Task
 from exceptions import ConnectionError
-
 
 
 class Queue(object):
@@ -41,118 +39,118 @@ class Queue(object):
     passes optional config dictionary with details for Redis
     server, it will connect to that instance. By default it connects
     to the localhost.
-    
+
     """
-    def __init__(self, name, config = {}):
+    def __init__(self, name, config={}):
         self.name = name
-        self._name = 'retaskqueue-' + name 
+        self._name = 'retaskqueue-' + name
         if not config:
-            self.config = {'host':'localhost', 'port':6379, 'db':0,\
-                           'password':None}
+            self.config = {'host': 'localhost', 'port': 6379, 'db': 0,\
+                           'password': None}
         else:
             self.config = config
         self.rdb = None
         self.connected = False
-        
-    @property   
+
+    @property
     def length(self):
         """
         Gives the length of the queue. Returns ``None`` if the queue is not
         connected.
-        
+
         If the queue is not connected then it will raise
         :class:`retask.ConnectionError`.
-        
+
         """
         if not self.connected:
             raise ConnectionError('Queue is not connected')
-        
+
         try:
             length = self.rdb.llen(self._name)
         except redis.exceptions.ConnectionError, err:
             raise ConnectionError(str(err))
-        
+
         return length
-    
+
     def connect(self):
         """
         Creates the connection with the redis server.
         Return ``True`` if the connection works, else returns
         ``False``. It does not take any arguments.
-    
+
         :return: ``Boolean`` value
-          
+
         .. note::
-  
+
            After creating the ``Queue`` object the user should call
            the ``connect`` method so create the connection.
-  
+
         .. doctest::
-  
+
            >>> from retask.queue import Queue
            >>> q = Queue('test')
            >>> q.connect()
            True
-        
+
         """
         config = self.config
-        self.rdb = redis.Redis(config['host'], config['port'], config['db'], 
+        self.rdb = redis.Redis(config['host'], config['port'], config['db'],\
                               config['password'])
         try:
             info = self.rdb.info()
             self.connected = True
         except redis.ConnectionError:
             return False
-        
+
         return True
-          
+
     def dequeue(self):
         """
         Returns a :class:`~retask.task.Task` object from the queue. Returns ``None`` if the
         queue is empty.
-  
+
         :return: :class:`~retask.task.Task` object from the queue
-        
+
         If the queue is not connected then it will raise
         :class:`retask.ConnectionError`
-        
+
         .. doctest::
-  
+
            >>> from retask.queue import Queue
            >>> q = Queue('test')
            >>> q.connect()
            True
            >>> t = q.dequeue()
            >>> print t.data
-           {u'name': u'kushal'}        
+           {u'name': u'kushal'}
 
         """
         if not self.connected:
             raise ConnectionError('Queue is not connected')
-        
+
         if self.rdb.llen(self._name) == 0:
             return None
-        
+
         data = self.rdb.rpop(self._name)
         task = Task(data, True)
         return task
-    
+
     def enqueue(self, task):
         """
         Enqueues the given :class:`~retask.task.Task` object to the queue and returns
         a tuple. Value in index 0 is ``Boolean`` explaining the enqueue
-        operation is a success or not. Value at index 1 is string with 
+        operation is a success or not. Value at index 1 is string with
         error/success message (if any).
-        
+
         :arg task: ::class:`~retask.task.Task` object
-        
-        :return: Tuple with ``Boolean`` value and ``string`` message. 
-        
+
+        :return: Tuple with ``Boolean`` value and ``string`` message.
+
         If the queue is not connected then it will raise
         :class:`retask.ConnectionError`.
-        
+
         .. doctest::
-  
+
            >>> from retask.queue import Queue
            >>> q = Queue('test')
            >>> q.connect()
@@ -161,11 +159,11 @@ class Queue(object):
            >>> task = Task({'name':'kushal'})
            >>> q.enqueue(task)
            (True, 'Pushed')
-        
+
         """
         if not self.connected:
             raise ConnectionError('Queue is not connected')
-        
+
         if not task.data:
             return False, 'No data'
         try:
@@ -174,8 +172,8 @@ class Queue(object):
         except Exception, err:
             return False, str(err)
         return True, 'Pushed'
-    
+
     def __repr__(self):
             if not self:
                 return '%s()' % (self.__class__.__name__,)
-            return '%s(%r)' % (self.__class__.__name__, self.name)    
+            return '%s(%r)' % (self.__class__.__name__, self.name)
